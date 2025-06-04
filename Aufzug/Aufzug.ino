3770654e5ef31bfbca051aa4db8c4a337555ff99
Robotik <robotik@pdr-kelkheim.de>
2025-06-04 15:22:06 +0200

    Update Aufzug.ino
    M3(Lichtschranke) auskommentiert
    adresse im RIC 2

--- Aufzug/Aufzug.ino
+++ Aufzug/Aufzug.ino
@@ -3,7 +3,7 @@
 /*
 M1 Aufzug
 M2 Förderband
-M3 Licht-Lichtschranke
+M3 Licht-Lichtschranke füge hinzu wenn lcihtschranke verwendet wird
 M4 Förderband to HRL
 I1 Lichtschranke 
 I2 Stop unten
@@ -12,7 +12,7 @@ I3 Stop oben
 RobotikInterConnect *ric;
 
 void setup() {
-ric = new RobotikInterConnect(9);
+ric = new RobotikInterConnect(2);
 ric->send(255,"Connected to Interconnect");
 ric->send(255,"Setup");
 go_down();
@@ -23,20 +23,20 @@ void loop() {
 ric->send(255,"READY");
 String msg = ric->recv();
   //DO STUFF
-  ftduino.motor_set(Ftduino::M3, Ftduino::LEFT);
-  delay(1500);
+  //ftduino.motor_set(Ftduino::M3, Ftduino::LEFT);
+  //delay(1500);
   while (ftduino.input_get(Ftduino::I1)){
     ftduino.motor_set(Ftduino::M2, Ftduino::LEFT);
   }
   ftduino.motor_set(Ftduino::M2, Ftduino::OFF);
   ftduino.motor_set(Ftduino::M1, Ftduino::OFF);
-  ftduino.motor_set(Ftduino::M3, Ftduino::OFF);
+  //ftduino.motor_set(Ftduino::M3, Ftduino::OFF);
   go_up();
   ftduino.motor_set(Ftduino::M2, Ftduino::LEFT);
   ftduino.motor_set(Ftduino::M4, Ftduino::LEFT);
-  delay(500);
-  ric->send(10, msg);
-  delay(4000);
+  delay(3000);
+  ric->send(3, msg);//adress of arm
+  delay(5000);
   ftduino.motor_set(Ftduino::M2, Ftduino::OFF);    
   ftduino.motor_set(Ftduino::M4, Ftduino::OFF);
   go_down();
