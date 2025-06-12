e3c502b0e86365c65cc146b8631536809ee4230c
Robotik <106907231+pdrRobotik@users.noreply.github.com>
2025-06-12 16:55:13 +0200

    push

--- Aufzug/Aufzug.ino
+++ Aufzug/Aufzug.ino
@@ -21,38 +21,18 @@ ric->send(255,"Setup completed");
 }
 void loop() {
 
-ric->send(255,"READY");
-String msg = ric->recv();
-  //DO STUFF
-  //ftduino.motor_set(Ftduino::M3, Ftduino::LEFT);
-  //delay(1500)
-ftduino.motor_set(Ftduino::M2, Ftduino::LEFT);
-  delay(3000);
-  
-  ftduino.motor_set(Ftduino::M1, Ftduino::RIGHT); //Auftzug hoch
-  if (ftduino.input_get(Ftduino::I3)){
-    ftduino.motor_set(Ftduino::M1, Ftduino::OFF);
-  }
- ftduino.motor_set(Ftduino::M2, Ftduino::LEFT);
- ftduino.motor_set(Ftduino::M4, Ftduino::LEFT);
- delay(8000);
-ftduino.motor_set(Ftduino::M2, Ftduino::OFF);
-
-ftduino.motor_set(Ftduino::M1, Ftduino::LEFT); //Aufzug RUNter
-  if (ftduino.input_get(Ftduino::I2)){
-    ftduino.motor_set(Ftduino::M1, Ftduino::OFF);
-  }
-
-
-//tooooooooooooonys code
-  //ftduino.motor_set(Ftduino::M3, Ftduino::OFF);
+  ric->send(255,"READY");
+  String msg = ric->recv();
+  ftduino.motor_set(Ftduino::M2, Ftduino::LEFT);
+  delay(2000);//roll time till going up
+  ftduino.motor_set(Ftduino::M2, Ftduino::OFF);
   go_up();
   ftduino.motor_set(Ftduino::M2, Ftduino::LEFT);
   ftduino.motor_set(Ftduino::M4, Ftduino::LEFT);
-  delay(3000);
-  ric->send(3, msg);//adress of arm
+  delay(4000);
+  ftduino.motor_set(Ftduino::M2, Ftduino::OFF);     
   delay(5000);
-  ftduino.motor_set(Ftduino::M2, Ftduino::OFF);    
+  ric->send(3,"msg");
   ftduino.motor_set(Ftduino::M4, Ftduino::OFF);
   go_down();
 }
